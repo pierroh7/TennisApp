db30b0140b0144855c3091524dfa98ca1cd9826a
pierroh7 <pierre.miroite@edu.ece.fr>
2020-03-07 22:55:56 +0100

    [ADD] - OnClickListeners on Layouts

--- app/src/main/java/com/example/mytennisapp/MainActivity.java
+++ app/src/main/java/com/example/mytennisapp/MainActivity.java
@@ -2,13 +2,45 @@ package com.example.mytennisapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    private LinearLayout map, photo, score;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        map = findViewById(R.id.map);
+        photo = findViewById(R.id.photo);
+        score = findViewById(R.id.score);
+
+        map.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        photo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        score.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ScoreActivity.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
